67bf048d103594a1cbbf26e42ce85f378eda78e2
underscoremone <underscoremone@gmail.com>
2017-04-04 15:59:31 +0100

    gtexswifi: BoardConfig: Add architecture
    Change-Id: I687a5147c48006e390d97a035ee7a6bfdea1a820

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,6 +2,14 @@ LOCAL_PATH := device/samsung/gtexswifi
 
 TARGET_OTA_ASSERT_DEVICE := SM-T280,gtexswifi
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := SC7730SW
 
