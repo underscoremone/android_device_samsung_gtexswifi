297829f6862a5400ba24c7cc439df41d13cbff76
underscoremone <underscoremone@gmail.com>
2020-09-14 01:58:21 +0100

    gtexswifi: Add sprd_sign and use it

--- bootimg.mk
+++ bootimg.mk
@@ -3,6 +3,8 @@ LOCAL_PATH := $(call my-dir)
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 INSHEAD := $(LOCAL_PATH)/insertheader/prebuilt/bin/imgheaderinsert
 SIGNEDIMG ?= $(PRODUCT_OUT)/recovery-sign.img
+SPRDSIGN := $(LOCAL_PATH)/signimage/prebuilt/bin/sprd_sign
+SPRDSIGNCFG := $(LOCAL_PATH)/signimage/config
 
 ifdef TARGET_PREBUILT_DTB
 	BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DTB)
@@ -17,5 +19,6 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(re
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	$(hide) $(INSHEAD) $(INSTALLED_RECOVERYIMAGE_TARGET) 1
 	$(hide) mv $(SIGNEDIMG) $(INSTALLED_RECOVERYIMAGE_TARGET)
+	$(hide) $(SPRDSIGN) $(INSTALLED_RECOVERYIMAGE_TARGET) $(SPRDSIGNCFG)
 	$(hide) tar -C $(PRODUCT_OUT) -H ustar -c recovery.img > $(FLASH_IMAGE_TARGET)
 	@echo -e ${CL_CYN}"Made Odin flashable recovery tar: ${FLASH_IMAGE_TARGET}"${CL_RST}
